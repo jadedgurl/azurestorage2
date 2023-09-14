862068e9be210fe6e20279f7c45cd72e4a01f8d8
jadedgurl <126923594+jadedgurl@users.noreply.github.com>
2023-09-13 20:08:55 -0400

    Add files via upload

---
+++ main (1).tf
@@ -0,0 +1,18 @@
+#This is an Azure Montreal College Tutorial for Storage Account creation--->Storage Container name Creation--->Storage Blob Creation
+
+resource "azurerm_resource_group" "azureresourcegroup" {
+  name     = "MCIT_resource_group"
+  location = "Canada Central"
+}
+
+resource "azurerm_storage_account" "azureresourcegroup" {
+  name                     = "saj1storage1account"
+  resource_group_name      = azurerm_resource_group.azureresourcegroup.name
+  location                 = azurerm_resource_group.azureresourcegroup.location
+  account_tier             = "Standard"
+  account_replication_type = "GRS"
+
+  tags = {
+    environment = "staging"
+  }
+}
